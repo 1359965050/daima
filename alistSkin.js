0dc9a87e39f1a982fd2aae01959d7c75377a0c8f
xianren <85949064+1359965050@users.noreply.github.com>
2022-03-18 21:08:59 +0800

    Update alistSkin.js

--- alistSkin.js
+++ alistSkin.js
@@ -1,52 +1,42 @@
-/*
- * name : Future
- * edition : 2.0.2
- * message : 前端美化样式由星环[Future]提供，程序由Alist创始者友情提供
- * technical support : https://www.xhto.cn/
- */
-
-var x = true;
-
-$("html").click(function() {
-    if (x) {
-        var title = document.title,
-            // 个人信息编写区域+星环博客提供技术支持+ —————— ON
-
-            // 头像链接地址
-            headimg = "",
-            // 博主铭言句子
-            motto = "道法自然",
-            // 配置 - 导航内容及链接【注：名称和链接需要一一对应，用英文逗号隔开】
-            linkname = ["首页", "星环博客"],
-            link = ["/", "https://www.xhto.cn/"],
-            code = "";
-
-        // 个人信息编写区域+星环博客提供技术支持+ —————— END
-
-        // 程序化代码
-        code += '<h3>' + title + '</h3>';
-        code += '<div >< img src = "' + headimg + '" alt = "[ ' + title + ' ]" / >< /div>';
-        code += '<p>' + motto + '</p >';
-        code += '< ul >';
-        for (vo in linkname) {
-            code += '<li> < a href = "' + link[vo] + '" > ' + linkname[vo] + ' < /a></li >';
-        }
-        code += '< /ul>';
-
-        // 代码写入视觉程序
-        $(".header").append(code);
-    }
-})
-
-// 支持样式代码
-$(document).on('click', '.buttons>span:eq(1) svg', function() {
-    var $viwe = $(this).attr('viewBox');
-    alert($viwe);
-})
-
-window.onload = function() {
-    setTimeout(function() {
-        $("html").click();
-        x = false;
-    }, 100);
+.chakra-ui-light{
+  background-image: url("https://www.dmoe.cc/random.php") !important;
+  background-repeat:no-repeat;background-size:cover;background-attachment:fixed;background-position-x:center;
 }
+.main-box {
+  border-radius: 15px !important;
+}
+.chakra-ui-light .main-box {
+  background-color: #ffffff70 !important;
+}
+.chakra-ui-light .readme-box {
+  background-color: white !important;
+}
+.readme-box {
+  border-radius: 15px !important;
+}
+</style>
+
+<script src="https://eqcn.ajz.miesnfu.com/wp-content/plugins/wp-3d-pony/live2dw/lib/L2Dwidget.min.js"></script>
+
+<script>
+    L2Dwidget.init({
+        "model": {
+            jsonPath: "https://blog.imlete.cn/live2d/assets/weier.model.json",<!--这里改模型，前面后面都要改-->
+            "scale": 0.8
+        },
+        "display": {
+            "position": "right",<!--设置看板娘的上下左右位置-->
+            "width": 50,
+            "height": 100,
+            "hOffset": 50,
+            "vOffset": -10
+        },
+        "mobile": {
+            "show": true,
+            "scale": 0.5
+        },
+        "react": {
+            "opacityDefault": 1,<!--设置透明度-->
+            "opacityOnHover": 0.2
+        }
+    });
